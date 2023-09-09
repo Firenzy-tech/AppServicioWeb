820b57f8df234374251d041201944634c491862a
JUAN CARLOS ESCOBAR GIL <juanescobar290070@correo.itm.edu.co>
2023-09-09 16:27:07 -0500

    Start Proyect

---
+++ EmployeeOOP/EmployeeOOP/Classes/Date.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.Design;
+
+namespace EmployeeOOP.Classes
+{
+    public class Date
+    {
+
+
+        private int _year;
+        private int _month;
+        private int _day;
+
+        public Date(int day, int month, int year)
+        {
+            _day = day;
+            _month = ValidateMonth(month);
+            _year = year;
+        }
+
+        private static int ValidateMonth(int month)
+        {
+          if (month <= 1 && month >= 12 )
+            {
+                return month;
+            }
+          else
+            {
+                 throw new (string.Format("el mes no es valido {0}", month));
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return @$"{_day:00}/{_month:00}/{_year:0000}";
+
+        }
+
+    }
+}
---
+++ EmployeeOOP/EmployeeOOP/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EmployeeOOP.Classes;
+
+Console.WriteLine("OPP APLICATION");
+Console.WriteLine("________________");
+
+Date objectDate = new (4, 5, 2023);
+
+Console.WriteLine(objectDate.ToString());
+
+
